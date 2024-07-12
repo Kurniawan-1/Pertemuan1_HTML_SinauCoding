149d868853e3c8b3427c7e0cb5cf4b981be6b4a9
Kurniawan-1 <kurniawanaja784@gmail.com>
2024-07-12 16:43:32 +0700

    push tugas pertemuan8

---
+++ pertemuan8/src/views/ViewTable.vue
@@ -0,0 +1,24 @@
+<script setup>
+import { reactive } from "vue";
+import TableComponent from "../components/ComponentTable.vue";
+import Header from "../json/Header.json";
+import User from "../json/User.json";
+
+const tableData = reactive({
+  header: Header,
+  data: User,
+});
+
+const handleShowData = (data) => {
+  alert(
+    `Nama ${data.nama}, Posisi ${data.position}, No Telp. ${data.no_telp} , Email ${data.email} `
+  );
+};
+</script>
+
+<template>
+  <div>
+    <h3>Table View</h3>
+    <TableComponent :dataTable="tableData" @showData="handleShowData" />
+  </div>
+</template>
